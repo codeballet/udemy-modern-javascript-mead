1b377773e69459473a7821a1bae8765e1cd0013a
johan <stjernholm@msn.com>
2020-02-12 16:42:00 +0800

    Section 6.42: Arrays - Searching Arrays Part I

--- 6-arrays/notes.js
+++ 6-arrays/notes.js
@@ -1,4 +1,13 @@
-const notes = ['Note 1', 'Note 2', 'Note 3']
+const notes = [{
+  title: 'My next trip',
+  body: 'I would like to go to Spain'
+}, {
+  title: 'Habits to work on',
+  body: 'Exercise. Eating a bit better.'
+}, {
+  title: 'Office modifications',
+  body: 'Get a new seat'
+}]
 
 // console.log(notes.pop())
 // notes.push('My new note')
@@ -8,21 +17,26 @@ const notes = ['Note 1', 'Note 2', 'Note 3']
 
 // notes.splice(1, 1, 'This is the new second item')
 
-notes[2] = 'This is now the new note 3'
+// notes[2] = 'This is now the new note 3'
 
-notes.forEach(function (item, index) {
-  console.log(index)
-  console.log(item)
-})
+// notes.forEach(function (item, index) {
+//   console.log(index)
+//   console.log(item)
+// })
+
+// for (let count = 0; count <= 2; count++) {
+//   console.log(count)
+// }
+
+// for (let count = notes.length - 1; count >= 0; count--) {
+//   console.log(notes[count])
+// }
 
 console.log(notes.length)
 console.log(notes)
 
-// Counting...
-for (let count = 0; count <= 2; count++) {
-  console.log(count)
-}
-
-for (let count = notes.length - 1; count >= 0; count--) {
-  console.log(notes[count])
-}
+const index = notes.findIndex(function (note, index) {
+  console.log(note)
+  return note.title === 'Habits to work on'
+})
+console.log(index)
