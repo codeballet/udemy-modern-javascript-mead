ab3a2c09db9ca6aca9827d26c68920886f718e4a
johan <stjernholm@msn.com>
2020-02-14 22:15:25 +0800

    Section 7.52: Browser - Adding Elements via the DOM

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -1,7 +1,37 @@
-const ps = document.querySelectorAll('p')
+const todos = [{
+  text: 'Learn JavaScript',
+  completed: false
+}, {
+  text: 'Let the dogs out',
+  completed: true
+}, {
+  text: 'Write the dance syllabus',
+  completed: false
+}, {
+  text: 'Buy food',
+  completed: false
+}, {
+  text: 'Check the Taobao deliveries',
+  completed: true
+}, {
+  text: 'Talk to Gary',
+  completed: true
+}]
 
-ps.forEach(function (p) {
-  if (p.textContent.includes('the')) {
-    return p.remove()
-  }
+// You have 2 todos left (p element)
+// Add a p for each todo above (use text value)
+
+const incompleteTodos = todos.filter(function (todo) {
+  return !todo.completed
+})
+
+const summary = document.createElement('h2')
+summary.textContent = `You have ${incompleteTodos.length} todos left.`
+document.querySelector('body').appendChild(summary)
+
+
+todos.forEach(function (todo) {
+  const p = document.createElement('p')
+  p.textContent = todo.text
+  document.querySelector('body').appendChild(p)
 })
