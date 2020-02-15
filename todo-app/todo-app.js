3d32ac97ac748c07fa504c02fbbab75c8fb8db82
johan <stjernholm@msn.com>
2020-02-15 23:12:28 +0800

    Section 7.58: Browser - Working with Forms

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -19,27 +19,58 @@ const todos = [{
 }]
 
 
-const incompleteTodos = todos.filter(function (todo) {
-  return !todo.completed
+// Filters
+const filters = {
+  searchText: ''
+}
+
+// Render Todos list based on filters
+const renderTodos = function (todos, filters) {
+  const filteredTodos = todos.filter(function (todo) {
+    return todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
+  })
+
+  const incompleteTodos = filteredTodos.filter(function (todo) {
+    return !todo.completed
+  })
+  
+  document.querySelector('#todos').innerHTML = ''
+  
+  const summary = document.createElement('h2')
+  summary.textContent = `You have ${incompleteTodos.length} todos left.`
+  document.querySelector('#todos').appendChild(summary)
+
+  
+  filteredTodos.forEach(function (todo) {
+    const p = document.createElement('p')
+    p.textContent = todo.text
+    document.querySelector('#todos').appendChild(p)
+  })
+}
+
+
+// Listeners
+document.querySelector('#search-todos').addEventListener('input', function (e) {
+  filters.searchText = e.target.value
+  renderTodos(todos, filters)
 })
 
-const summary = document.createElement('h2')
-summary.textContent = `You have ${incompleteTodos.length} todos left.`
-document.querySelector('body').appendChild(summary)
-
-
-todos.forEach(function (todo) {
-  const p = document.createElement('p')
-  p.textContent = todo.text
-  document.querySelector('body').appendChild(p)
+document.querySelector('#new-todo').addEventListener('submit', function (e) {
+  e.preventDefault()
+  todos.push({
+    text: e.target.elements.todoText.value,
+    completed: false
+  })
+  renderTodos(todos, filters)
+  e.target.elements.todoText.value = ''
 })
 
-// Listen for new todo text input
-document.querySelector('#new-todo-text').addEventListener('input', function (e) {
-  console.log(e.target.value)
-})
 
-// Listen for new todo button click
-document.querySelector('#add-todo').addEventListener('click', function () {
-  console.log('New Todo button was clicked')
-})
+// Function initiators
+renderTodos(todos, filters)
+
+// 1. Create a form with a single input for todo text
+// 2. Setup an submit handler and cancel the default action
+// 3. Add a new item to the todos array with that text data (completed value of false)
+// 4. Re-render the application
+// 5. Clear the input field value
