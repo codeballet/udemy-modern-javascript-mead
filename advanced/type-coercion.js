8c7a167ac4f367cff15a9a0a4a4c2d50cc522a93
johan <stjernholm@msn.com>
2020-02-19 11:06:27 +0800

    Section 9.84: Type Coercion

---
+++ advanced/type-coercion.js
@@ -0,0 +1,11 @@
+// Type Coercion - a string, a number, a boolean
+// === takes type into account - use instead of ==
+
+// console.log('5' + 5)
+// console.log('5' - 5)
+// console.log('5' === 5)
+
+const value = true + 12
+const type = typeof value
+console.log(type)
+console.log(value)
