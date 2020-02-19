7b18fad3a79c5126fba4052046b34bff3e43b1c1
johan <stjernholm@msn.com>
2020-02-19 21:26:09 +0800

    Section 10.90: Constructor Functions

---
+++ oop/hangman.js
@@ -0,0 +1,10 @@
+const Hangman = function (word, remainingGuesses) {
+  this.word = word,
+  this.remainingGuesses = remainingGuesses
+}
+
+const game1 = new Hangman('cat', 2)
+const game2 = new Hangman('Sweden', 3)
+
+console.log(game1)
+console.log(game2)
