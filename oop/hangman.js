e8bf34bc7a83a286dbc89c8a651d658ec6a33c7d
johan <stjernholm@msn.com>
2020-02-20 22:38:26 +0800

    Section 10.93: Digging into Prototypical Inheritance

--- oop/hangman.js
+++ oop/hangman.js
@@ -1,10 +1,21 @@
 const Hangman = function (word, remainingGuesses) {
-  this.word = word
+  this.word = word.toLowerCase().split('')
   this.remainingGuesses = remainingGuesses
+  this.guessedLetters = ['c', 'e']
+}
+
+Hangman.prototype.getPuzzle = function () {
+  let puzzle = ''
+
+  this.word.forEach((letter) => {
+    this.guessedLetters.includes(letter) || letter === ' ' ? puzzle += letter : puzzle += '*'
+  })
+
+  return puzzle
 }
 
 const game1 = new Hangman('cat', 2)
-const game2 = new Hangman('Sweden', 3)
+console.log(game1.getPuzzle())
 
-console.log(game1)
-console.log(game2)
+const game2 = new Hangman('My name', 3)
+console.log(game2.getPuzzle())
