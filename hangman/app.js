bf1eaa34e60f68a1c288bdd30165cc5e4e50a43b
johan <stjernholm@msn.com>
2020-02-21 10:33:05 +0800

    Section 10.95: Primitives and Objects Part II

--- hangman/app.js
+++ hangman/app.js
@@ -1,11 +1,15 @@
-// product --> Object.prototype --> null
+// Primitive value: string, number, boolean, null, undefined
+// Everything else are objects.
 
-const product = new Object({
-  name: 'The War of Art'
-})
+// Object: myObject --> Object.prototype --> null
+// Array: myArray --> Array.prototype --> Object.prototype --> null
+// Function: myFunc --> Function.prototype --> Object.prototype --> null
+// String: myString --> String.prototype --> Object.prototype --> null
+// Number: myNumber --> Number.prototype --> Object.prototype --> null
+// Boolean: myBoolean --> Boolean.prototype --> Object.prototype --> null
 
-Object.prototype.someNewMethod = () => 'This is new function'
-
-// hasOwnProperty
-console.log(product.someNewMethod('hasOwnProperty')) // false
+const product = 'Computer'
 console.log(product)
+
+const otherProduct = new String('Phone')
+console.log(otherProduct)
