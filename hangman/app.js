9cde6fb1c475ad9c3965a03f0c98ebed767f4d54
johan <stjernholm@msn.com>
2020-02-22 16:34:00 +0800

    Section 11.106: Exploring API restcountries.eu

--- hangman/app.js
+++ hangman/app.js
@@ -9,7 +9,7 @@ const game1 = new Hangman('car parts', 2)
 puzzleEl.textContent = game1.puzzle
 statusMessageEl.textContent = game1.statusMessage
 
-window.addEventListener('keypress', function (e) {
+window.addEventListener('keypress', (e) => {
   const guess = String.fromCharCode(e.charCode)
   game1.makeGuess(guess)
   puzzleEl.textContent = game1.puzzle
@@ -20,12 +20,31 @@ window.addEventListener('keypress', function (e) {
 // Making an HTTP request
 const request = new XMLHttpRequest()
 
-request.addEventListener('readystatechange', (e) => {
-  if (e.target.readyState === 4) {
+// request.addEventListener('readystatechange', (e) => {
+//   if (e.target.readyState === 4 && e.target.status === 200) {
+//     const data = JSON.parse(e.target.responseText)
+//     console.log(data)
+//   } else if (e.target.readyState === 4) {
+//     console.log('An error has taken place')
+//   }
+// })
+
+// request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=3')
+// request.send()
+
+// Challenge
+const countryCode = 'CN'
+const countryRequest = new XMLHttpRequest()
+
+countryRequest.addEventListener('readystatechange', (e) => {
+  if (e.target.readyState === 4 && e.target.status === 200) {
     const data = JSON.parse(e.target.responseText)
-    console.log(data)
+    const country = data.find((country) => country.alpha2Code === countryCode)
+    console.log(country.name)
+  } else if (e.target.readyState === 4) {
+    console.log('Unable to fetch data')
   }
 })
 
-request.open('GET', 'http://puzzle.mead.io/puzzle')
-request.send()
+countryRequest.open('GET', 'http://restcountries.eu/rest/v2/all')
+countryRequest.send()
