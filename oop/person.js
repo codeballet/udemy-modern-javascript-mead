1afa545722a7022598fe26e79bfccaaee79fcbb7
johan <stjernholm@msn.com>
2020-02-22 10:47:57 +0800

    Section 10.100: Creating Subclasses

--- oop/person.js
+++ oop/person.js
@@ -1,4 +1,5 @@
 // Prototypal inheritance
+// myPerson --> Person.prototype --> Object.prototype --> null
 
 class Person {
   constructor(firstName, lastName, age, likes = []) {
@@ -26,9 +27,48 @@ class Person {
 }
 
 
-const me = new Person('Andrew', 'Mead', 27, ['Teaching', 'Biking'])
-me.setName('Alexis Turner')
-console.log(me.getBio())
+class Employee extends Person {
+  constructor(firstName, lastName, age, position, likes) {
+    super(firstName, lastName, age, likes)
+    this.position = position
+  }
 
-const person2 = new Person('Johan', 'Stjernholm', 42)
-console.log(person2.getBio())
+  getBio() {
+    return `${this.firstName} ${this.lastName} is a ${this.position}.`
+  }
+
+  getYearsLeft() {
+    return 65 - this.age
+  }
+}
+
+
+class Student extends Person {
+  constructor(firstName, lastName, age, grade, likes) {
+    super(firstName, lastName, age, likes)
+    this.grade = grade
+  }
+
+  getBio() {
+    const status = this.grade >= 70 ? 'passing' : 'failing'
+    return `${this.firstName} is ${status} the class.`
+  }
+
+  updateGrade(change) {
+    this.grade += change
+  }
+}
+
+
+const anne = new Student('Anne', 'Lindt', 18, 73)
+console.log(anne.getBio())
+anne.updateGrade(-10)
+console.log(anne.getBio())
+
+// const me = new Employee('Andrew', 'Mead', 27, 'Teacher', ['Teaching', 'Biking'])
+// me.setName('Alexis Turner')
+// console.log(me.getBio())
+// console.log(me.getYearsLeft())
+
+// const person2 = new Person('Johan', 'Stjernholm', 42)
+// console.log(person2.getBio())
