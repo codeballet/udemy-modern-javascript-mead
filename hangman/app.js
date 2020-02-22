0135a56f12ce0cd5115e0da6e3a76de6f3159b62
johan <stjernholm@msn.com>
2020-02-22 17:11:52 +0800

    Section 11.107: Callback Abstraction

--- hangman/app.js
+++ hangman/app.js
@@ -1,7 +1,3 @@
-// HTTP Hypertext Transfer Protocol
-// Request - What do we want to do
-// Response - What was actually done
-
 const puzzleEl = document.querySelector('#puzzle')
 const statusMessageEl = document.querySelector('#status-message')
 const game1 = new Hangman('car parts', 2)
@@ -16,35 +12,30 @@ window.addEventListener('keypress', (e) => {
   statusMessageEl.textContent = game1.statusMessage
 })
 
-
-// Making an HTTP request
-const request = new XMLHttpRequest()
-
-// request.addEventListener('readystatechange', (e) => {
-//   if (e.target.readyState === 4 && e.target.status === 200) {
-//     const data = JSON.parse(e.target.responseText)
-//     console.log(data)
-//   } else if (e.target.readyState === 4) {
-//     console.log('An error has taken place')
-//   }
-// })
-
-// request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=3')
-// request.send()
-
-// Challenge
-const countryCode = 'CN'
-const countryRequest = new XMLHttpRequest()
-
-countryRequest.addEventListener('readystatechange', (e) => {
-  if (e.target.readyState === 4 && e.target.status === 200) {
-    const data = JSON.parse(e.target.responseText)
-    const country = data.find((country) => country.alpha2Code === countryCode)
-    console.log(country.name)
-  } else if (e.target.readyState === 4) {
-    console.log('Unable to fetch data')
+getPuzzle((error, puzzle) => {
+  if (error) {
+    console.log(`Error: ${error}`)
+  } else {
+    console.log(puzzle)
   }
 })
 
-countryRequest.open('GET', 'http://restcountries.eu/rest/v2/all')
-countryRequest.send()
+
+// Making an HTTP request
+
+// Challenge
+// const countryCode = 'CN'
+// const countryRequest = new XMLHttpRequest()
+
+// countryRequest.addEventListener('readystatechange', (e) => {
+//   if (e.target.readyState === 4 && e.target.status === 200) {
+//     const data = JSON.parse(e.target.responseText)
+//     const country = data.find((country) => country.alpha2Code === countryCode)
+//     console.log(country.name)
+//   } else if (e.target.readyState === 4) {
+//     console.log('Unable to fetch data')
+//   }
+// })
+
+// countryRequest.open('GET', 'http://restcountries.eu/rest/v2/all')
+// countryRequest.send()
