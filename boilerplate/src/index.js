389f4fc100db9376e1f52cd0c85b216c8d23d6bf
johan <stjernholm@msn.com>
2020-02-26 18:45:46 +0800

    Section 13.140: Converting Hangman App

--- boilerplate/src/index.js
+++ boilerplate/src/index.js
@@ -1,8 +1,2 @@
-import otherSquare, { add, name } from './utilities'
-import otherScream from './scream'
-
-console.log('index.js')
-console.log(add(32, 3))
-console.log(name)
-console.log(otherScream(name))
-console.log(otherSquare(10))
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
