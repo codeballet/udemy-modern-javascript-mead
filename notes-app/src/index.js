4bd6a89928a6047ff58a1dae6d78d33f5a2a0147
johan <stjernholm@msn.com>
2020-02-27 12:11:13 +0800

    Section 13.144: Converting Notes App part III

--- notes-app/src/index.js
+++ notes-app/src/index.js
@@ -1,7 +1,21 @@
 import 'core-js/stable'
 import 'regenerator-runtime/runtime'
-import { getNotes, createNote } from './notes'
+import { getNotes, createNote, removeNote, updateNote } from './notes'
+import { getFilters, setFilters } from './filters'
 
-console.log(getNotes())
-createNote()
-console.log(getNotes())
+// console.log(getNotes())
+// createNote()
+// removeNote('')
+
+// updateNote('32a67846-53e4-4325-9561-ff96d5aa1e2e', {
+//   title: 'My note title',
+//   body: 'The body for my note.'
+// })
+// console.log(getNotes())
+
+console.log(getFilters())
+setFilters({
+  searchText: 'Office',
+  sortBy: 'byCreated'
+})
+console.log(getFilters())
