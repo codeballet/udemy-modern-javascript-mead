9d0b7eabcd88a60b00d61a8e0a224be36c20dcb8
johan <stjernholm@msn.com>
2020-03-02 11:09:46 +0800

    Section 13.150: The Rest and Spread Syntax

--- boilerplate/src/index.js
+++ boilerplate/src/index.js
@@ -1,2 +1,31 @@
 import 'core-js/stable'
 import 'regenerator-runtime/runtime'
+
+// const calculateAverage = (thing, ...numbers) => {
+//   let sum = 0
+//   numbers.forEach((num) => sum += num)
+//   const average = sum / numbers.length
+//   return `The average ${thing} is ${average}`
+// }
+
+// console.log(calculateAverage('age', 0, 100, 88, 64))
+
+const printTeam = (team, coach, ...players) => {
+  console.log(`Team: ${team}`)
+  console.log(`Coach: ${coach}`)
+  console.log(`Players: ${players.join(', ')}`)
+}
+
+const team = {
+  name: 'Liberty',
+  coach: 'Casey Penn',
+  players: ['Marge', 'Aiden', 'Herbert', 'Sherry']
+}
+
+printTeam(team.name, team.coach, ...team.players)
+
+
+let cities = ['Barcelone', 'Cape Town', 'Bordeuax']
+cities = ['Stockholm', ...cities]
+
+console.log(cities)
